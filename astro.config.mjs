ec7e9e042b002d7699d65f99f48f0c9521073209
MARTIN MATUS <martinmatus100@gmail.com>
2025-11-13 17:13:16 -0300

    Setup GitHub Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import github from "@astrojs/github";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://martinmatus100.github.io/cava24-demo", 
-  integrations: [svelte(), github()],
-  outDir: "dist",
+  integrations: [svelte()],
+  site: "https://martinmatus100.github.io",
+  base: "/cava24-demo",
 });
